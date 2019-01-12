999d5c60b378f2598487a25d09c4b4816e9218dc
nqui <imdangodaane@gmail.com>
2019-01-12 21:42:33 +0700

    my own branch

--- old_metro_rush.py
+++ old_metro_rush.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 import collections
 from sys import stderr
-from operator import itemgetter
 import time
-import re
 
 
 class Station:
@@ -44,13 +42,6 @@ class Train:
     def change_line(self, next_line):
         self.current_line = next_line
 
-    def check_and_pick_route(self, routes):
-        if self.route is None:
-            for i in range(len(routes)):
-                if len(routes[i][1].trains) == 0:
-                    self.route = routes[i]
-                    return None
-
 
 class Map:
     def __init__(self, file_name):
@@ -80,83 +71,46 @@ class Map:
             self.trains.append((i+1, train))
             self.start.add_train(i+1, train)
 
-    def atoi(self, text):
-        return int(text) if text.isdigit() else text
-
-    def natural_keys(self, text):
-        return [ self.atoi(c) for c in re.split('(\d+)', text) ]
-
-    def print_status(self):
-        for i, route in enumerate(self.routes):
-            station_names = [station.name for station in route]
-            for station in route:
-                ind = station_names.index(station.name)
-                if len(station.trains) != 0:
-                    trains = []
-                    station_names[ind] = "\033[1;32m" + station.name + "\033[0;0m"
-                    for id, train in station.trains:
-                        trains.append(train.name)
-                    trains.sort(key=self.natural_keys)
-                    trains = ','.join(trains)
-                    print(station_names[ind] + '(' + station.line + ':' +
-                          station.id + ')' + '-' + trains)
-                    # print(station.name + '(' + station.line + ':' +
-                    #       station.id + ')' + '-' + trains)
-                else:
-                    station_names[ind] = "\033[0;0m" + station.name
+    def print_status(self, route):
+        for station in route:
+            print(station.name)
+            for id, train in station.trains:
+                print(train.name, end=" ")
             print()
-            print("\033[3;31mRoute " + str(i+1) + ":\033[0;0m")
-            # print("\033[1;30m => \033[0;0m".join(station.name for station in route))
-            print("\033[1;30m => \033[0;0m".join(i for i in station_names))
-            print('=========================================')
 
     def metro_run_train(self):
         turn = 0
+        route = self.routes[0]
         home = set()
-        keep_running = True
 
-        while keep_running:
-            print()
-            print("\033[1;33m" + "Turn: " + str(turn+1) + "\033[0;0m", end=' ')
-            print("\033[1;36m(づ￣ ³￣)づ (づ￣ ³￣)づ (づ￣ ³￣)づ (づ￣ ³￣)づ (づ￣ ³￣)づ \033[0;0m")
-            print()
+        route_name = [i.name for i in route]
+        tmp = " => ".join(route_name)
+        while len(route[-1].trains) != int(self.total_trains):
+            print("Turn: " + str(turn) + " Route: (" + tmp + ")")
+            self.print_status(route)
             for id, train in self.trains:
-                train.check_and_pick_route(self.routes)
-                try:
-                    if id in home:
-                        continue
-                    current_pos = train.route.index(train.current_station)
-                    if train.route[current_pos] == train.route[-1]:
-                        home.add(id)
-                        continue
-                    next_station = train.route[current_pos + 1]
-                    if train.current_station.name in self.transfer_points:
-                        if next_station.line != train.current_line:
-                            train.change_line(next_station.line)
-                            continue
-                    if next_station == train.route[-1] or len(next_station.trains) == 0:
-                        train.move_to(next_station)
-                        next_station.add_train(id, train)
-                        train.route[current_pos].remove_train(id, train)
-                except AttributeError:
+                if id in home:
                     continue
-            self.print_status()
-            count = 0
-            for id, train in self.trains:
-                if train.current_station.name == self.end.name:
-                    count += 1
-            if count == int(self.total_trains):
-                keep_running = False
+                current_pos = route.index(train.current_station)
+                if route[current_pos] == route[-1]:
+                    home.add(id)
+                    continue
+                next_station = route[current_pos + 1]
+                if train.current_station.name in self.transfer_points:
+                    if next_station.line != train.current_line:
+                        train.change_line(next_station.line)
+                        continue
+                if next_station == route[-1] or len(next_station.trains) == 0:
+                    train.move_to(next_station)
+                    next_station.add_train(id, train)
+                    route[current_pos].remove_train(id, train)
             turn += 1
-            time.sleep(0.01)
+            time.sleep(0.1)
+        print("Turn: " + str(turn) + " Route: (" + tmp + ")")
+        self.print_status(route)
+        print("===========================================")
         print()
-        print("\033[1;1mTotal turn = " + str(turn))
-        print()
-        print("\033[1;33m(づ￣ ³￣)づ (づ￣ ³￣)づ (づ￣ ³￣)づ (づ￣ ³￣)づ (づ￣ ³￣)づ\033[0;0m")
-        print()
-        print("\033[1;34m(づ｡◕‿‿◕｡)づ (づ｡◕‿‿◕｡)づ (づ｡◕‿‿◕｡)づ (づ｡◕‿‿◕｡)づ (づ｡◕‿‿◕｡)づ\033[0;0m")
-        print()
-
+        print("Total turn = " + str(turn))
 
     def append_line(self, line):
         self.map.append(line)
@@ -198,13 +152,6 @@ class Map:
                 else:
                     _, self.total_trains = i.split("=")
 
-    def add_station_neighbor(self, home, nb_info, nb_line_name):
-        for line in self.map:
-            for station in line.stations:
-                if station.name == nb_info[1] and station.line == nb_line_name:
-                    self.station_names[home].append(station)
-                    return None
-
     def find_neighbor(self, content):
         for i in range(len(content)):
             if content[i].startswith('#'):
@@ -218,20 +165,24 @@ class Map:
                 try:
                     if content[i-1][:1].isdigit():
                         info = content[i-1].split(":")
-                        self.add_station_neighbor(name, (info[0], info[1]), line.name)
+                        station = Station(info[0],info[1],line.name)
+                        self.station_names[name].append(station)
                     if content[i+1][:1].isdigit():
                         info = content[i+1].split(":")
-                        self.add_station_neighbor(name, (info[0], info[1]), line.name)
+                        station = Station(info[0],info[1],line.name)
+                        self.station_names[name].append(station)
                 except IndexError:
                     continue
                 except KeyError:
                     self.station_names[name] = []
                     if content[i-1][:1].isdigit():
                         info = content[i-1].split(":")
-                        self.add_station_neighbor(name, (info[0], info[1]), line.name)
+                        station = Station(info[0],info[1],line.name)
+                        self.station_names[name].append(station)
                     if content[i+1][:1].isdigit():
                         info = content[i+1].split(":")
-                        self.add_station_neighbor(name, (info[0], info[1]), line.name)
+                        station = Station(info[0],info[1],line.name)
+                        self.station_names[name].append(station)
 
     def find_start_and_end(self):
         for i in range(len(self.map)):
@@ -244,6 +195,7 @@ class Map:
                     self.end = self.map[i].stations[j]
 
     def find_route_bfs(self):
+        self.routes = []
         queue = collections.deque([[self.start]])
         seen = set()
         seen.add(self.start.name)
@@ -260,12 +212,11 @@ class Map:
                 if nearby.name not in seen:
                     queue.append(path + [nearby])
                     seen.add(nearby.name)
+        return self.routes
 
     def print_routes(self):
         routes = self.routes
         for i, route in enumerate(routes):
-            for i in range(len(route)):
-                station.trains.sort()
             print("Route " + str(i+1) + ":")
             print("\033[1;30m => \033[0;0m".join(station.name for station in route))
 
@@ -275,9 +226,12 @@ class Map:
 
 def main():
     m = Map('delhi-metro-stations')
-    # m = Map('file')
     m.bfs()
     m.metro_run_train()
+    # for line in m.map:
+        # print("\033[4;31m" + line.name + ":" + "\033[0m")
+        # for station in line.stations:
+        #     print(station.name)
 
 
 if __name__ == '__main__':
